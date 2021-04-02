162df9d0d9d545f885fac376360949b04b22da58
mvplab-yonsei <80297707+mvplab-yonsei@users.noreply.github.com>
2021-04-02 17:45:19 +0900

    Add files via upload

--- Segmentation/test.py
+++ Segmentation/test.py
@@ -29,20 +29,26 @@ def get_size(bytes, suffix="B"):
         bytes /= factor
 
 
-def write_info(logger):
-    uname = platform.uname()
-    logger.info("======================================Boot Time======================================")
-    logger.info(f"Boot Time: {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
-    logger.info("==================================System Information==================================")
-    logger.info(f"System: {uname.system}\nNode Name: {uname.node}\nRelease: {uname.release}\nVersion: {uname.version}\nMachine: {uname.machine}\nProcessor: {uname.processor}")
-    svmem = psutil.virtual_memory()
-    nvmlInit()
-    handle = nvmlDeviceGetHandleByIndex(0)
-    logger.info(f"CPU: {cpuinfo.cpu.info[0]['model name']}\nMemory total: {get_size(svmem.total)}\nGPU: {nvmlDeviceGetName(handle)}")
+def write_info(logger, is_started):
+    if is_started:
+        uname = platform.uname()
+        logger.info(f"Test Command: 'python test.py'")
+        logger.info("======================================Boot Time======================================")
+        logger.info(f"Boot Time: {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
+        logger.info("==================================System Information==================================")
+        logger.info(f"System: {uname.system}\nNode Name: {uname.node}\nRelease: {uname.release}\nVersion: {uname.version}\nMachine: {uname.machine}\nProcessor: {uname.processor}")
+        svmem = psutil.virtual_memory()
+        nvmlInit()
+        handle = nvmlDeviceGetHandleByIndex(0)
+        logger.info(f"CPU: {cpuinfo.cpu.info[0]['model name']}\nMemory total: {get_size(svmem.total)}\nGPU: {nvmlDeviceGetName(handle)}")
+        logger.info(f"Pytorch Version: {torch.__version__}")
+    else:
+        logger.info("======================================Boot Time======================================")
+        logger.info(f"Boot Time: {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
+
 
 
 def Logger_System(xml_dir, log_dir, output, paths, threshold):
-
     xml_file = os.path.join(xml_dir, paths.split('/')[-1].replace('jpg', 'xml'))
     log_file = os.path.join(log_dir, paths.split('/')[-1].replace('jpg', 'txt'))
 
@@ -65,21 +71,17 @@ def Logger_System(xml_dir, log_dir, output, paths, threshold):
 
 class Tester(object):
     def __init__(self, use_logger, epoch):
-        # _, self.val_loader = make_data_loader()
         self.val_loader = make_data_loader()
         self.model = smp.PAN(encoder_name='resnet34', encoder_weights='imagenet', in_channels=3, classes=2).cuda()
         self.model = nn.DataParallel(self.model)
         self.model.load_state_dict(torch.load('./trained_model/{}_34.tar'.format(epoch)))
         self.model.eval()
-        self.xml_dir = '/data1/database/data_0310/xmls'
-        self.log_dir = '/data1/submit_code/Logger'
-        self.paths = '/data1/submit_code/Test_code_segmentation/testset/images/'
-        self.threshold = 2048
         self.use_logger = use_logger
-        os.mkdir('./output')
-        os.mkdir('./output/img')
-        os.mkdir('./output/pred')
-        os.mkdir('./output/mask')
+        if not os.path.isdir('./output'):
+            os.mkdir('./output')
+            os.mkdir('./output/img')
+            os.mkdir('./output/pred')
+            os.mkdir('./output/mask')
 
     def testing(self):
         total_acc = 0
@@ -105,8 +107,12 @@ class Tester(object):
 
             # Calculate Pixels
             if self.use_logger:
+                xml_dir = '../../database/data_0310/xmls'
+                log_dir = '../Logger'
+                paths = './testset/images/'
                 Crack_pixels = torch.count_nonzero(FG_pred)
-                Logger_System(self.xml_dir, self.log_dir, Crack_pixels, self.paths + name[0], self.threshold)
+                threshold = 2 ** 10
+                Logger_System(xml_dir, log_dir, Crack_pixels, paths + name[0], threshold)
 
             # dilation kernel for pixel margin
             margin = 2
@@ -115,7 +121,9 @@ class Tester(object):
             dil_BG_pred = torch.clamp(torch.nn.functional.conv2d(BG_pred, kernel, padding=(margin, margin)), 0, 1)
 
             # evaluate
-            pixel_acc = torch.sum(FG_pred * mask + BG_pred * (1 - mask)) / mask.size(-1) / mask.size(-2)
+            total_correct_pixels = torch.sum(FG_pred * mask + BG_pred * (1 - mask))
+            total_pixels = mask.size(-1) * mask.size(-2)
+            pixel_acc = total_correct_pixels / total_pixels
             total_acc += pixel_acc
             FG_total = torch.sum((FG_pred + mask).clamp(max=1))
             FG_correct = torch.sum(dil_FG_pred * mask)
@@ -124,12 +132,14 @@ class Tester(object):
             miou = (FG_correct / FG_total + BG_correct / BG_total) / 2
             total_miou += miou
 
-            logger.info('[{}] {} [Pixel accuracy:{:5f}, mIou:{:5f}]'.format(str(i+1), name[0], pixel_acc, miou))
+            logger.info('[{}] {} [Pixal Acc.:{:5f}] [mIoU:{:5f}]'.format(str(i+1), name[0], pixel_acc, miou))
+            logger.info('(total # of pixels:{}, total # of correct pixels:{}, FG Union:{}, FG Intersection:{}, BG Union:{}, BG Intersection:{})'.
+                        format(int(total_pixels), int(total_correct_pixels), int(FG_total), int(FG_correct), int(BG_total), int(BG_correct)))
 
-            # save results
-            tv.utils.save_image((image * 0.2) + 0.5, './output/img/{}.png'.format(i))
-            tv.utils.save_image(FG_pred, './output/pred/{}.png'.format(i))
-            tv.utils.save_image(mask.float(), './output/mask/{}.png'.format(i))
+            # # save results
+            # tv.utils.save_image((image * 0.2) + 0.5, './output/img/{}.png'.format(i))
+            # tv.utils.save_image(FG_pred, './output/pred/{}.png'.format(i))
+            # tv.utils.save_image(mask.float(), './output/mask/{}.png'.format(i))
 
         # testing
         total_acc /= (i + 1)
@@ -139,16 +149,14 @@ class Tester(object):
 
 def main():
     torch.cuda.set_device(0)
-
     logger = setup_logger("Test", './', 0)
-    write_info(logger)
-
+    write_info(logger, True)
     parser = argparse.ArgumentParser(description="Road Segmentation Test")
     parser.add_argument("--use_logger_system", action='store_true')
-
     args = parser.parse_args()
-    trainer = Tester(args.use_logger_system, epoch=40)
+    trainer = Tester(args.use_logger_system, epoch=25)
     trainer.testing()
+    write_info(logger, False)
 
 
 if __name__ == "__main__":
